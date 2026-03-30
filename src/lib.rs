1e2eeebe465a9a8996b8dc33e42ab66c822bd92c
jeremiah <jeremiahbl@protonmail.com>
2026-03-30 03:31:16 -0500

    lib.rs: code cleanup, optimization, bugfixes, and refractoring all in one giant ugly commit!

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub mod image;
 #[cfg(feature = "sdl")]
 pub mod sdl;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 pub struct RGBA {
 	pub r: u8,
 	pub g: u8,
@@ -18,7 +18,7 @@ pub struct RGBA {
 	pub a: u8,
 }
 
-pub trait ReprColor: Sized + Copy {
+pub trait ReprColor: Sized + Copy + PartialEq {
 	fn from_rgb(r: u8, g: u8, b: u8) -> Self;
 	fn to_rgb(&self) -> [u8; 3];
 
@@ -44,62 +44,13 @@ impl ReprColor for RGBA {
 pub trait GraphicsOperations: GraphicsRenderer {
 	/* For branch prediction reasons, all of these are assumed to
 	 * have appropriate input values, and if not you're fucked */
-	fn clear(&mut self, col: Self::Color);
-	fn rect(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32);
-	fn rect_outline(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32);
-	fn circle(&mut self, col: Self::Color, x: u32, y: u32, s: u32);
-	fn circle_outline(&mut self, col: Self::Color, x: u32, y: u32, s: u32);
-	fn ellipse(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32);
-	fn ellipse_outline(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32);
-	fn line(&mut self, col: Self::Color, x: u32, y: u32, x2: u32, y2: u32);
-	fn hline_bounded(&mut self, col: Self::Color, x: u32, y1: u32, y2: u32);
-	fn vline_bounded(&mut self, col: Self::Color, y: u32, x1: u32, x2: u32);
-	fn hline(&mut self, col: Self::Color, x: u32);
-	fn vline(&mut self, col: Self::Color, y: u32);
-}
-
-pub trait GraphicsRenderer {
-	type Color: ReprColor;
-
-	fn get_pixel(&self, x: u32, y: u32) -> Self::Color;
-	fn get_width(&self) -> u32;
-	fn get_height(&self) -> u32;
-	fn get_size(&self) -> (u32, u32) {
-		(self.get_width(), self.get_height())
-	}
-	fn get_num_pixels(&self) -> u32 {
-		self.get_width() * self.get_height()
-	}
-
-	fn set_pixel(&mut self, col: Self::Color, x: u32, y: u32);
-	fn set_pixels(&mut self, pixels: &Vec<Self::Color>) {
-		let w = self.get_width();
-		let h = self.get_height();
-		assert!(
-			pixels.len() == self.get_num_pixels() as usize,
-			"Given incorrect amount of pixels, given {}, got {}!",
-			pixels.len(),
-			self.get_num_pixels() as usize,
-		);
-		for y in 0..h {
-			let p = y * w;
-			for x in 0..w {
-				self.set_pixel(pixels[(x + p) as usize], x, y);
-			}
+	fn clear(&mut self, col: Self::Color) {
+		/* chose to iterate over y here since most displays
+		 * draw on a row-by-row / scan line basis */
+		for p in 0..self.get_height() {
+			self.hline(col, p);
 		}
 	}
-	fn blend_pixel(&mut self, col: Self::Color, x: u32, y: u32, alpha: u8) {
-		let (old, new) = (self.get_pixel(x, y).to_rgb(), col.to_rgb());
-		let alpha16 = alpha as u16;
-		let alpha_orig = 256_u16 - alpha16;
-		let r = ((old[0] as u16 * alpha_orig) + (new[0] as u16 * alpha16)) / 256;
-		let g = ((old[1] as u16 * alpha_orig) + (new[1] as u16 * alpha16)) / 256;
-		let b = ((old[2] as u16 * alpha_orig) + (new[2] as u16 * alpha16)) / 256;
-		self.set_pixel(Self::Color::from_rgb(r as u8, g as u8, b as u8), x, y);
-	}
-}
-
-impl<T: GraphicsRenderer> GraphicsOperations for T {
 	fn rect(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32) {
 		for xi in 0..w {
 			for yi in 0..h {
@@ -121,23 +72,35 @@ impl<T: GraphicsRenderer> GraphicsOperations for T {
 		self.ellipse_outline(col, x, y, s, s)
 	}
 	fn ellipse(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32) {
-		let wh = w / 2;
-		let hh = h / 2;
-		let _dw2 = wh * wh;
-		let dh2 = hh * hh;
-		for xp in 0..wh {
-			let xp2 = xp * xp;
-			for yp in 0..hh {
-				if xp2 + yp * yp < dh2 {
-					self.set_pixel(col, x - xp, y - yp);
-					self.set_pixel(col, x + xp, y - yp);
-					self.set_pixel(col, x - xp, y + yp);
-					self.set_pixel(col, x + xp, y + yp);
+		let hh = h * h;
+		let ww = w * w;
+		let hhww = hh * ww;
+		let mut x0 = w;
+		let mut dx = 0;
+		let (mut x0, mut dx, hhww) = (w, 0, hh * ww);
+
+		for yp in 0..h {
+			let mut x1 = x0 - (dx - 1);
+			while x1 > 0 {
+				if x1 * x1 * hh + yp * yp * ww <= hhww {
+					break;
 				}
+				x1 -= 1;
+			}
+
+			// approximate the slope
+			dx = x0 - x1;
+			x0 = x1;
+
+			for xp in 0..x0 {
+				self.set_pixel(col, x + xp, y + yp);
+				self.set_pixel(col, x + xp, y - yp);
+				self.set_pixel(col, x - xp, y + yp);
+				self.set_pixel(col, x - xp, y - yp);
 			}
 		}
 	}
-	fn ellipse_outline(&mut self, _col: Self::Color, _x: u32, _y: u32, _w: u32, _h: u32) {
+	fn ellipse_outline(&mut self, col: Self::Color, x: u32, y: u32, w: u32, h: u32) {
 		//todo
 	}
 	fn line(&mut self, col: Self::Color, x1: u32, y1: u32, x2: u32, y2: u32) {
@@ -152,30 +115,64 @@ impl<T: GraphicsRenderer> GraphicsOperations for T {
 			self.set_pixel(col, xp as u32, yp as u32);
 		}
 	}
+	fn hline_bounded(&mut self, col: Self::Color, yp: u32, x1: u32, x2: u32) {
+		assert!(x2 > x1, "x2 must be greater then x1 for hline_bounded!");
+		for xp in x1..x2 {
+			self.set_pixel(col, xp, yp);
+		}
+	}
 	fn vline_bounded(&mut self, col: Self::Color, xp: u32, y1: u32, y2: u32) {
 		assert!(y2 > y1, "y2 must be greater then y1 for vline_bounded!");
 		for yp in y1..y2 {
 			self.set_pixel(col, xp, yp);
 		}
 	}
-	fn hline_bounded(&mut self, col: Self::Color, yp: u32, x1: u32, x2: u32) {
-		assert!(x2 > x1, "x2 must be greater then x1 for vline_bounded!");
-		for xp in x1..x2 {
-			self.set_pixel(col, xp, yp);
-		}
+	fn hline(&mut self, col: Self::Color, p: u32) {
+		self.hline_bounded(col, p, 0, self.get_width());
 	}
 	fn vline(&mut self, col: Self::Color, p: u32) {
 		self.vline_bounded(col, p, 0, self.get_height());
 	}
-	fn hline(&mut self, col: Self::Color, p: u32) {
-		self.hline_bounded(col, p, 0, self.get_width());
+}
+
+pub trait GraphicsRenderer {
+	type Color: ReprColor;
+
+	fn get_pixel(&self, x: u32, y: u32) -> Self::Color;
+	fn get_width(&self) -> u32;
+	fn get_height(&self) -> u32;
+	fn get_size(&self) -> (u32, u32) {
+		(self.get_width(), self.get_height())
 	}
-	fn clear(&mut self, col: Self::Color) {
-		/* chose to iterate over y here since most displays
-		 * draw on a row-by-row / scan line basis */
-		for p in 0..self.get_height() {
-			self.hline(col, p);
+	fn get_num_pixels(&self) -> u32 {
+		self.get_width() * self.get_height()
+	}
+
+	fn set_pixel(&mut self, col: Self::Color, x: u32, y: u32);
+	fn set_pixels(&mut self, pixels: &Vec<Self::Color>, x1: u32, y1: u32, x2: u32, y2: u32) {
+		let w = self.get_width();
+		let h = self.get_height();
+		assert!(
+			pixels.len() == self.get_num_pixels() as usize,
+			"Given incorrect amount of pixels, given {}, got {}!",
+			pixels.len(),
+			self.get_num_pixels() as usize,
+		);
+		for y in y1..y2 {
+			let p = y * w;
+			for x in x1..x2 {
+				self.set_pixel(pixels[(x + p) as usize], x, y);
+			}
 		}
+	}
+	fn blend_pixel(&mut self, col: Self::Color, x: u32, y: u32, alpha: u8) {
+		let (old, new) = (self.get_pixel(x, y).to_rgb(), col.to_rgb());
+		let alpha16 = alpha as u16;
+		let alpha_orig = 256_u16 - alpha16;
+		let r = ((old[0] as u16 * alpha_orig) + (new[0] as u16 * alpha16)) / 256;
+		let g = ((old[1] as u16 * alpha_orig) + (new[1] as u16 * alpha16)) / 256;
+		let b = ((old[2] as u16 * alpha_orig) + (new[2] as u16 * alpha16)) / 256;
+		self.set_pixel(Self::Color::from_rgb(r as u8, g as u8, b as u8), x, y);
 	}
 }
 
@@ -185,6 +182,10 @@ pub struct BufferedRenderer<T: GraphicsRenderer> {
 	base: T,
 	buffer_width: u32,
 	buffer_height: u32,
+	buffer_x_min: u32,
+	buffer_y_min: u32,
+	buffer_x_max: u32,
+	buffer_y_max: u32,
 	buffer: Vec<T::Color>,
 }
 
@@ -194,6 +195,10 @@ impl<T: GraphicsRenderer> BufferedRenderer<T> {
 			base: base_renderer,
 			buffer_width: 0,
 			buffer_height: 0, /* these are set by resize_to_base */
+			buffer_x_min: 0,
+			buffer_y_min: 0,
+			buffer_x_max: 0,
+			buffer_y_max: 0,
 			buffer: Vec::new(),
 		};
 		out.resize_to_base();
@@ -203,10 +208,15 @@ impl<T: GraphicsRenderer> BufferedRenderer<T> {
 	pub fn resize_to_base(&mut self) {
 		self.buffer_width = self.base.get_width();
 		self.buffer_height = self.base.get_height();
+		self.buffer_x_max = self.buffer_width;
+		self.buffer_y_max = self.buffer_height;
+		(self.buffer_x_min, self.buffer_y_min) = (0, 0);
+
 		self.buffer = Vec::<T::Color>::with_capacity(self.base.get_num_pixels() as usize);
 		for _i in 0..self.base.get_num_pixels() {
 			self.buffer.push(T::Color::new(0, 0, 0));
 		}
+
 		println!(
 			"BufferedRenderer size set to {} x {} ({} pixels)",
 			self.buffer_width,
@@ -216,7 +226,16 @@ impl<T: GraphicsRenderer> BufferedRenderer<T> {
 	}
 
 	pub fn push_buffer(&mut self) {
-		self.base.set_pixels(&self.buffer)
+		self.base.set_pixels(
+			&self.buffer,
+			self.buffer_x_min,
+			self.buffer_y_min,
+			self.buffer_x_max,
+			self.buffer_y_max,
+		);
+		(self.buffer_x_max, self.buffer_y_max) = (0, 0);
+		self.buffer_x_min = self.buffer_width;
+		self.buffer_y_min = self.buffer_height;
 	}
 }
 
@@ -236,9 +255,17 @@ impl<T: GraphicsRenderer> GraphicsRenderer for BufferedRenderer<T> {
 
 	fn set_pixel(&mut self, col: T::Color, x: u32, y: u32) {
 		let w = self.get_width();
-		self.buffer[(x + y * w) as usize] = col;
+		if self.buffer[(x + y * w) as usize] != col {
+			self.buffer_x_min = self.buffer_x_min.min(x);
+			self.buffer_y_min = self.buffer_y_min.min(y);
+			self.buffer_x_max = self.buffer_x_max.max(x + 1);
+			self.buffer_y_max = self.buffer_y_max.max(y + 1);
+			self.buffer[(x + y * w) as usize] = col;
+		}
 	}
 }
+
+impl<T: GraphicsRenderer> GraphicsOperations for BufferedRenderer<T> {}
 
 // Multi display horizontal renderer
 
@@ -258,6 +285,16 @@ impl<T: GraphicsRenderer, const N: usize> MultiDisplayHorizontalRenderer<T, N> {
 			base_h,
 		};
 		out
+	}
+
+	fn set_pixels_impl(&mut self, pixels: &Vec<T::Color>, x1: u32, y1: u32, x2: u32, y2: u32) {
+		let w = self.get_width();
+		for y in y1..y2 {
+			let p = y * w;
+			for x in x1..x2 {
+				self.set_pixel(pixels[(x + p) as usize], x, y);
+			}
+		}
 	}
 }
 
@@ -282,5 +319,33 @@ impl<T: GraphicsRenderer, const N: usize> GraphicsRenderer
 		let i = x / self.base_w;
 		let p = x - i * self.base_w;
 		self.renderers[i as usize].set_pixel(col, p, y);
+	}
+
+	fn set_pixels(&mut self, pixels: &Vec<Self::Color>, x1: u32, y1: u32, x2: u32, y2: u32) {
+		let w = self.get_width();
+		let h = self.get_height();
+		assert!(
+			pixels.len() == self.get_num_pixels() as usize,
+			"Given incorrect amount of pixels, given {}, got {}!",
+			pixels.len(),
+			self.get_num_pixels() as usize,
+		);
+		for i in 0..(self.renderers.len() as u32) {
+			let rx1 = i * self.base_w;
+			let rx2 = rx1 + self.base_w;
+			if x1 < rx2 {
+				self.set_pixels_impl(pixels, x1.max(rx1), y1, x2.min(rx2), y2);
+			}
+		}
+	}
+}
+
+impl<T: GraphicsRenderer + GraphicsOperations, const N: usize> GraphicsOperations
+	for MultiDisplayHorizontalRenderer<T, N>
+{
+	fn clear(&mut self, col: T::Color) {
+		for rend in &mut self.renderers {
+			rend.clear(col);
+		}
 	}
 }
